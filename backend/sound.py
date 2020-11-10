3e005e3c6a1c6e783b92d47b04c8d5091c4355af
senthu-Laptop <senthuu27@gmail.com>
2020-11-10 11:58:15 +0100

    Add logic to create a file which outputs current environment sound

--- backend/sound.py
+++ backend/sound.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python
+
 import RPi.GPIO as GPIO
 import time
+import json
 
+# sound_pin wird definiert
 sound_pin = 18
-
+# GPIO mode wird auf GPIO.BOARD gesetzt
 GPIO.setmode(GPIO.BOARD)
-
+# sound_pin wird als Eingang festgelegt
 GPIO.setup(sound_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 try:
     while True:
-    if (GPIO.input(sound_pin) == GPIO.LOW):
-        print('Sound erkannt')
-    time.sleep(0.1)
+        # ueberpruefe ob ein Geraeusch erkannt wird
+        if (GPIO.input(sound_pin) == GPIO.LOW):
+            data = {
+                "sound": {
+                    "volume": 1
+                }
+            }
+            with open("example.json", "w") as write_file:
+                json.dump(data, write_file)
+            time.sleep(0.5)
+        if (GPIO.input(sound_pin) == GPIO.HIGH):
+            data = {
+                "sound": {
+                    "volume": 1
+                }
+            }
+            with open("example.json", "w") as write_file:
+                json.dump(data, write_file)
+            time.sleep(0.5)
+        else:
+            data = {
+                "sound": {
+                    "volume": 1
+                }
+            }
+        time.sleep(0.5)
+        with open("example.json", "w") as write_file:
+            json.dump(data, write_file)
 except KeyboardInterrupt:
-    # Strg+c beendet das Programm
     GPIO.cleanup()
